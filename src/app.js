b0fa84f37c2f39ce81a154a748e7036415837816
AashishRautela <asisrautela612@gmail.com>
2025-10-24 17:01:31 +0530

    Update CORS configuration to allow requests from Netlify deployment

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://tak-management.netlify.app/'],
     credentials: true
   })
 );
